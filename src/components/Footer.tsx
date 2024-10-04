6353de2ff701ed2c5ffcdb556182105cdae1cab3
Rahulmandyal1 <rahulmandyal079@gmail.com>
2024-10-04 10:16:57 +0530

    removed light theme related changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = (): React.ReactNode => {
   return (
-    <div className="flex items-center justify-center mt-2 py-6">
+    <div className="flex items-center justify-center mt-2 py-6 text-white">
       © 2024 Rahul Mandyal. All rights reserved.
     </div>
   );
