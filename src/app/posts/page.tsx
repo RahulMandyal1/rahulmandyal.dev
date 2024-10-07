4f10506bf6abd66d6aa9acc44af6458dcd898487
rahulmandayal1 <rahulthakurcoder@gmail.com>
2024-10-07 10:12:18 +0530

    🚧 feat: implement Blog component with dynamic slug-based rendering, 404 handling, and content display using Mdx and Balancer

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,34 +1,33 @@
-import { BlogCard } from "@/components/BlogCard";
 import { allBlogs } from "contentlayer/generated";
-import Link from "next/link";
-import { ENV } from "@/libs/env";
+import Balancer from "react-wrap-balancer";
+import NotFound from "@/components/NotFound";
+import { Mdx } from "@/components/Mdx";
+import { formatDate } from "@/libs/utils";
 
-const isProd = ENV.NODE_ENV === "production";
+export async function generateStaticParams() {
+  const paths = allBlogs.map((blog) => ({ slug: blog.slug }));
 
-export default function Blog() {
-  const blogs = allBlogs.sort((a, b) => {
-    if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
-      return -1;
-    }
-    return 1;
-  });
+  return paths;
+}
 
-  const undraftedBlogs = isProd ? blogs.filter((blog) => !blog.draft) : blogs;
+export default async function Blog({ params }: { params: { slug: string } }) {
+  const blog = allBlogs.find((blog) => blog.slug === params.slug);
+
+  if (!blog) {
+    return <NotFound />;
+  }
 
   return (
     <section>
-      <ul>
-        {undraftedBlogs.map((blog) => (
-          <li
-            key={blog.slug}
-            className="py-1 divide-y divide-gray-200 dark:divide-gray-700"
-          >
-            <Link href={`/blog/${blog.slug}`}>
-              <BlogCard blog={blog} />
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <h1 className="text-2xl font-bold tracking-tighter">
+        <Balancer>{blog.title}</Balancer>
+      </h1>
+      <div className="mb-8 mt-2 flex items-center justify-between text-sm text-neutral-600 dark:text-neutral-400">
+        <p>
+          {formatDate(blog.publishedAt)} - {blog.readingTime.text}
+        </p>
+      </div>
+      <Mdx code={blog.body.code} />
     </section>
   );
 }
