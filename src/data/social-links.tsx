25221bbfe5eab8406b7d4cde23130d57bb26f208
rahulmandayal1 <rahulthakurcoder@gmail.com>
2024-10-08 09:54:59 +0530

    Added Social Links :memo:

--- src/data/social-links.tsx
+++ src/data/social-links.tsx
@@ -8,22 +8,22 @@ import {
 export const SOCIALS = [
   {
     label: "GitHub",
-    href: "/",
+    href: "https://github.com/RahulMandyal1",
     icon: GitHubIcon,
   },
   {
     label: "X",
-    href: "/",
+    href: "https://x.com/RahulMandyal4",
     icon: XIcon,
   },
   {
     label: "LinkedIn",
-    href: "/",
+    href: "https://www.linkedin.com/in/rahul-thakur-0b2942213/",
     icon: LinkedInIcon,
   },
   {
     label: "Instagram",
-    href: "/",
+    href: "https://www.instagram.com/thakur_rahul079/",
     icon: InstagramIcon,
   },
 ];
