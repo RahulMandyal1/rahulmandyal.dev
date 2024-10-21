9816d5a2fa7749ed783284bf12a05b508ece7f54
rahulmandayal1 <rahulthakurcoder@gmail.com>
2024-10-21 09:49:29 +0530

    🔄 Updated bio

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -9,15 +9,21 @@ const AboutMe = () => {
       <h1 className="text-2xl font-bold">Rahul thakur</h1>
       <div>
         <p className="mt-4">
-          I’m a fullstack software engineer specializing in building scalable
-          web applications having rich user interface using javascript.
+          I am a fullstack software engineer specializing in building scalable
+          web applications with rich user interfaces using JavaScript. I have
+          worked with startups to deliver end-to-end solutions across various
+          domains, including Web3 platforms and crypto exchanges. My
+          contributions have played a key role in enhancing user experiences.
         </p>
         <p className="mt-4 mb-4">
-          I&apos;ve worked with startups to deliver end-to-end solutions across
-          various domains, including Web3 platforms and crypto exchanges. My
-          contributions have played a key role in enhancing user experiences.
-          &nbsp; Currently, I am building a SaaS platform, focusing on
-          delivering innovative and efficient software solutions.
+          I’ve contributed as a Frontend and React Native Developer at Torum
+          Technology and Technogetic Pvt. Ltd., developing web and mobile
+          products, including Web3 platforms and crypto exchanges. I’ve also
+          improved accessibility and enhanced platform security. Trained in MERN
+          stack development from altcampus, I’m currently pursuing a BCA from
+          Lovely Professional University. My tech stack includes HTML, CSS,
+          JavaScript, Node.js, Express.js, React.js, MongoDB, React Native, and
+          Xcode.
         </p>
 
         <p className="mb-4">
