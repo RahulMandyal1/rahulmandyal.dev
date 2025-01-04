cd9364fc7de74e1288fd5a8f836836e5c62714f2
Rahul Thakur <rahulmandyal079@gmail.com>
2025-01-04 23:05:05 +0530

    Updated copyright year to 2025.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer: React.FC = (): React.ReactNode => {
   return (
     <div className="flex items-center justify-center mt-2 py-6 text-white">
-      © 2024 Rahul Mandyal. All rights reserved.
+      © 2025 Rahul Mandyal. All rights reserved.  
     </div>
   );
 };
